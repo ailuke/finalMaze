b6b9bd0d57725000cf122d791829266004709f39
Pozdnako <pozdnako@uni.coventry.ac.uk>
2016-11-06 17:56:28 +0000

    Test file added

---
+++ Test.py
@@ -0,0 +1,15 @@
+#Setting up GUI
+'''
+from tkinter import *
+window = Tk()
+canvas = Canvas(window, width=650, height=650, bg='red')
+canvas.pack()
+window.mainloop()
+'''
+
+import time
+from tkinter import *
+window = Tk()
+canvas = Canvas(window, width=400, height=300, bg='white')
+canvas.pack()
+window.mainloop()
